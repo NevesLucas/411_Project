aaffd6ee4ef79f1eb3fdd9ed8159a5d66703bc18
NevesLucas <lneves@bu.edu>
2016-04-21 00:39:23 -0400

    switched from hapi to express, added login page

---
+++ app_api/config/config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var Akey = 'YOUR_API_KEY_HERE'
+module.exports = {
+	rootpath: path.normalize(__dirname+'/../..'),
+	server : {
+		host: 'localhost',
+		port: 3000
+	},
+	database : {
+		host: '127.0.0.1',
+		port: 27017,
+		db: 'flights',
+		username: '',
+		password: ''
+	},
+	qpx : {
+		endpoint : ' https://www.googleapis.com/qpxExpress/v1/trips',
+		key : Akey,
+		count: 20,
+		search: '/search'
+	}
+}
--- src/routes/index.js
+++
@@ -1,5 +0,0 @@
-﻿var jwt = require('express-jwt');
-var auth = jwt({
-    secret: 'MY_SECRET',
-    userProperty: 'payload'
-});
